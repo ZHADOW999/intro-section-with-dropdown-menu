f68fe28370946e4a30caee54b2a29344f24bca9f
ZHADOW999 <143715858+ZHADOW999@users.noreply.github.com>
2024-05-14 23:53:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,46 +4,12 @@ function myFunction() {
     
   }
   
-  // Close the dropdown menu if the user clicks outside of it
-//   window.onclick = function(event2) {
-//     if (!event2.target.matches('.dropdown-rel')) {
-//       var dropdowns = document.getElementsByClassName("dropdown");
-//       var i;
-//       for (i = 0; i < dropdowns.length; i++) {
-//         var openDropdown = dropdowns[i];
-//         if (openDropdown.classList.contains('show')) {
-//           openDropdown.classList.remove('show');
-         
-//         }
-//       }
-//     }
-//   }
-  
-
+ 
   function myFunction2() {
     document.getElementById("dropdown2").classList.toggle("show2");
     document.getElementById("arrow-2").classList.toggle("arrow-2");
     
   }
   
-  // Close the dropdown menu if the user clicks outside of it
-//  window.onclick = function(event1) {
-//     if (!event1.target.matches('.dropdown-rel-2')) {
-//       var dropdowns1 = document.getElementsByClassName("dropdown-2");
-//       var i;
-//       for (i = 0; i < dropdowns1.length; i++) {
-//         var openDropdown1 = dropdowns1[i];
-//         if (openDropdown1.classList.contains('show2') ) {
-            
-//           openDropdown1.classList.remove('show2');
-          
-//         }
-
-//         else if(openDropdown1.classList.contains('arrow-2')){
-//             openDropdown1.classList.remove('arrow-2');
-//         }
-        
-//       }
-//     }
-//   }
+ 
   
